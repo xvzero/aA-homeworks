97d727ef22b21a36d76c7b2bc1710b9770f0d942
Andre <25297095+xvzero@users.noreply.github.com>
2018-02-27 23:56:38 -0800

    Created migrations and models for people and houses

--- w3d3/db/migrate/20180228064611_create_people.rb
+++ w3d3/db/migrate/20180228064611_create_people.rb
@@ -1,10 +1,14 @@
 class CreatePeople < ActiveRecord::Migration[5.1]
   def change
+    #creates people table
     create_table :people do |t|
-      t.string :name
-      t.integer :house_id
+      t.string :name, null: false
+      t.integer :house_id, null: false
 
       t.timestamps
     end
+
+    #adds house_id to people table
+    add_index :people, :house_id
   end
 end
